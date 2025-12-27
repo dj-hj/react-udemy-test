f0bedffc81e6a7de98af37d743851c64d176e5a8
carmeloDjango <django.carmelo@gmail.com>
2025-12-27 09:16:41 +0900

    change github user

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 const App = () => {
   return (
     <>
-      <h1 style={{ color: "pink" }}>Learn React with KIWON.com</h1>
+      <h1 style={{ color: "pink" }}>Learn React with KIWON.com.surdsfl</h1>
       <p>This is sub heading this</p>
     </>
   );
